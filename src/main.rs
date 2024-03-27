66cdd556b1487e90af57bf8a11be87c3db646f59
NathanRoyer <nathan.royer.pro@gmail.com>
2024-03-27 20:35:50 +0100

    fix: remove cli for now

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use swat::parser::parse;
-
-fn main() {
-    let bytes = include_bytes!("../target/wasm32-unknown-unknown/release/swat.wasm");
-    let module = parse(bytes).unwrap();
-
-    println!("{:?}", module);
-}
